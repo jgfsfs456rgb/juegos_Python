a99b8281517a8180c167a239b763a78d38b22f08
José Gabriel Flores Salas <jgfsfs456@gmail.com>
2025-10-24 19:21:35 -0600

    Update juego_ahorcado.py

--- juego_ahorcado.py
+++ juego_ahorcado.py
@@ -1,42 +1,34 @@
 import random
-
-print("Bienvenido al juego del ahorcado tienes 3 vidas")
+print("Bienvenido al juego del ahorcado. Tienes 3 vidas.")
 print("Empezamos")
-def obtener_palabra_aleatoria():
-    palabras = ["telefono", "calculadora", "gato", "ingeniero", "software", "computacion"]
-    palabra_aleatoria = random.choice(palabras)
-    return palabra_aleatoria
-
-def mostrar_tablero(palabra_secreta, letras_adivinadas):
+palabras = ["telefono", "calculadora", "gato", "ingeniero", "software", "computacion"]
+palabra_secreta = random.choice(palabras)
+letras_adivinadas = []
+intentos = 3
+while intentos > 0:
     tablero = ""
     for letra in palabra_secreta:
         if letra in letras_adivinadas:
-            tablero+=letra
+            tablero += letra
         else:
-            tablero+="_"
-    print(tablero)
-        
-def jugar():
-    palabra_secreta=obtener_palabra_aleatoria()
-    letras_adivinadas=[]
-    intentos=3
-
-    while intentos>0:
-        mostrar_tablero(palabra_secreta, letras_adivinadas)
-        letra=input("introduce una letra:").lower()
-
-        if letra in letras_adivinadas:
-            print("Ya probaste esa letra ingresa otra")
-            continue
-        if letra in palabra_secreta:
-            letras_adivinadas.append(letra)
-            if set(letras_adivinadas)==set(palabra_secreta):
-             print("Haz acertado la palabra")
-             break
-        else:
-            intentos-=1
-            print(f"Letra incorrecta te quedan {intentos}")
-    if intentos==0:
-        print(f"perdiste la palabra era {palabra_secreta}")
-
-jugar()
+            tablero += "_"
+    print("Palabra:", tablero)
+    letra_ingresada = input("Introduce una letra: ").lower()
+    if letra_ingresada in letras_adivinadas:
+        print("Ya probaste esa letra, intenta con otra.")
+        continue
+    if letra_ingresada in palabra_secreta:
+        letras_adivinadas.append(letra_ingresada)
+        todas_adivinadas = True
+        for letra in palabra_secreta:
+            if letra not in letras_adivinadas:
+                todas_adivinadas = False
+                break
+        if todas_adivinadas:
+            print("¡Felicidades! Has acertado la palabra:", palabra_secreta)
+            break
+    else:
+        intentos -= 1
+        print(f"Letra incorrecta. Te quedan {intentos} intento(s).")
+if intentos == 0:
+    print(f"Has perdido. La palabra era: {palabra_secreta}")
